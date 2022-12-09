e8cdbb7ec2939da7bd61c34441d98ae5b3f9e623
da kong <kongda9999@gmail.com>
2022-12-09 10:32:46 +0800

    fix CI

--- server/scripts/serverless-deploy.js
+++ server/scripts/serverless-deploy.js
@@ -7,10 +7,12 @@ const execAsync = (cmd, options = {
   cwd: deployPath,
 }) => {
   return new Promise((resolve, reject) => {
-    exec(cmd, options, (code, stdout, stderr) => {
-      if (stderr) {
-        return reject(stderr);
+    exec(cmd, options, (error, stdout, stderr) => {
+      if (error) {
+        return reject(error);
       }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
       resolve(stdout);
     })
   });
@@ -22,20 +24,10 @@ async function run () {
   console.log(`run cmd: ${installCmd}`);
   const installRes = await execAsync(installCmd).catch((e) => console.log(e));
   console.log(installRes);
-  const serverlessDeployCmd = resolve(__dirname, '../node_modules/.bin/sls deploy --force --verbose');
+  const serverlessDeployCmd = '../node_modules/.bin/sls deploy --force --verbose';
   console.log(`run cmd: ${serverlessDeployCmd}`)
-  const serverlessDeployRes = await execAsync(serverlessDeployCmd).catch((e) => console.log(`Error: ${e}`));
+  const serverlessDeployRes = await execAsync(serverlessDeployCmd).catch((e) => console.log(e));
   console.log(serverlessDeployRes);
-  if (!serverlessDeployRes) {
-    return console.log('build fails');
-  }
-  const reg = /(https:\/\/.+\.amazonaws\.com).+\}/;
-  const arr = serverlessDeployRes.match(reg);
-  if (!arr || !arr[1]) {
-    return console.log('build fails');
-  }
-  const appUri = `${arr[1]}/prod`;
-  console.log(`App run in API gate way: ${appUri}`);
 }
 
 run();
