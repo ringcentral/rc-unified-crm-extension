03450469148c07cead6967bbab8f2f4462700de8
da kong <kongda9999@gmail.com>
2022-12-14 14:11:27 +0800

    fix CI

--- server/scripts/serverless-deploy.js
+++ server/scripts/serverless-deploy.js
@@ -26,7 +26,7 @@ async function run () {
   console.log(installRes);
   const serverlessDeployCmd = resolve(__dirname, '../node_modules/.bin/sls deploy --force --verbose');
   console.log(`run cmd: ${serverlessDeployCmd}`)
-  const serverlessDeployRes = await execAsync(serverlessDeployCmd).catch((e) => console.log(e));
+  const serverlessDeployRes = await execAsync(serverlessDeployCmd);
   console.log(serverlessDeployRes);
 }
 
