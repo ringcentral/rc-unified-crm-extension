ec6f078085630e66177ae43426da53d5bf66e215
da kong <kongda9999@gmail.com>
2023-08-29 12:26:08 +0800

    fix: pipedrive  C2D issue

--- client/public/options.js
+++ client/public/options.js
@@ -1,11 +1,11 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
     const region = document.getElementById('region').value;
-    const quickAccessButtonOn = document.getElementById('quickAccessButtonOn').value;
+    const c2dDelay = document.getElementById('c2dDelay').value;
     const overridingPhoneNumberFormat = document.getElementById('overridingPhoneNumberFormat').value;
 
     chrome.storage.local.set(
-        { selectedRegion: region, quickAccessButtonOn, overridingPhoneNumberFormat },
+        { selectedRegion: region, c2dDelay, overridingPhoneNumberFormat },
         () => {
             // Update status to let user know options were saved.
             const status = document.getElementById('status');
@@ -21,10 +21,10 @@ const saveOptions = () => {
 // stored in chrome.storage.
 const restoreOptions = () => {
     chrome.storage.local.get(
-        { selectedRegion: 'US', quickAccessButtonOn: 'ON', overridingPhoneNumberFormat: '' },
+        { selectedRegion: 'US', c2dDelay: '0', overridingPhoneNumberFormat: '' },
         (items) => {
             document.getElementById('region').value = items.selectedRegion;
-            document.getElementById('quickAccessButtonOn').value = items.quickAccessButtonOn;
+            document.getElementById('c2dDelay').value = items.c2dDelay;
             document.getElementById('overridingPhoneNumberFormat').value = items.overridingPhoneNumberFormat;
         }
     );
--- client/src/content.js
+++ client/src/content.js
@@ -43,7 +43,6 @@ async function initializeC2D() {
     },
   );
 }
-initializeC2D();
 
 // Listen message from background.js to open app window when user click icon.
 chrome.runtime.onMessage.addListener(
@@ -77,7 +76,6 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
 function Root() {
   return (
     <RcThemeProvider>
@@ -87,20 +85,25 @@ function Root() {
 }
 
 async function RenderQuickAccessButton() {
-  const { quickAccessButtonOn } = await chrome.storage.local.get(
-    { quickAccessButtonOn: 'ON' }
-  );
-  if (!window.location.hostname.includes('ringcentral.') && quickAccessButtonOn === 'ON') {
-    if (window.location.hostname.includes('pipedrive')) {
-      await delay(1000); // to prevent react hydration error on Pipedrive
-    }
+  if (!window.location.hostname.includes('ringcentral.')) {
     const rootElement = window.document.createElement('root');
     window.document.body.appendChild(rootElement);
     ReactDOM.render(<Root />, rootElement);
   }
 }
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-RenderQuickAccessButton();
+async function Initialize() {
+  const { c2dDelay } = await chrome.storage.local.get(
+    { c2dDelay: '0' }
+  );
+  const delayInMilliSec = Number(c2dDelay) * 1000;
+  await delay(delayInMilliSec);
+  await initializeC2D();
+  await RenderQuickAccessButton();
+}
+
+Initialize();
 
 if (window.location.pathname === '/pipedrive-redirect') {
   chrome.runtime.sendMessage({ type: "openPopupWindowOnPipedriveDirectPage", platform: 'pipedrive', hostname: 'temp' });
