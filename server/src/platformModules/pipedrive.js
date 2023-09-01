ba8b2fc7ac7c888e3271b5dbc57c33afa805d461
da kong <kongda9999@gmail.com>
2023-09-01 10:39:58 +0800

    0.6.6

--- server/src/platformModules/pipedrive.js
+++ server/src/platformModules/pipedrive.js
@@ -148,7 +148,7 @@ async function addMessageLog({ user, contactInfo, authHeader, message, additiona
         person_id: contactInfo.id,
         org_id: orgId,
         deal_id: dealId,
-        note: `<p>[Phone Number] ${contactNumber}</p><p>[Time] ${moment(message.creationTime).utcOffset(timezoneOffset).format('YYYY-MM-DD hh:mm:ss A')}</p>${!!message.subject ? `<p>[Message] ${message.subject}</p>` : ''} ${!!recordingLink ? `\n<p>[Recording link] ${recordingLink}</p>` : ''}`,
+        note: `<p>[Time] ${moment(message.creationTime).utcOffset(timezoneOffset).format('YYYY-MM-DD hh:mm:ss A')}</p>${!!message.subject ? `<p>[Message] ${message.subject}</p>` : ''} ${!!recordingLink ? `\n<p>[Recording link] ${recordingLink}</p>` : ''}`,
         done: true,
         due_date: timeUtc,
         due_time: dateUtc
