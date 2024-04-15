f7aebda7a433d698d7f986dd1bfbfbcaec4eb6ac
da kong <kongda9999@gmail.com>
2024-04-15 16:19:58 +0800

    fix: attempted fix in in-call note refresh issue

--- client/src/popup.js
+++ client/src/popup.js
@@ -251,9 +251,6 @@ window.addEventListener('message', async (e) => {
           }
           break;
         case "rc-active-call-notify":
-          if (data.call.telephonyStatus === 'CallConnected') {
-            window.postMessage({ type: 'rc-expandable-call-note-open', sessionId: data.call.sessionId }, '*');
-          }
           if (data.call.telephonyStatus === 'NoCall' && data.call.terminationType === 'final') {
             window.postMessage({ type: 'rc-expandable-call-note-terminate' }, '*');
           }
