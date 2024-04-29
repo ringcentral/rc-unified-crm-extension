70a86c4948b98a526846b7827d20be7dbe1dc46c
Da Kong <kongda9999@gmail.com>
2024-04-29 11:45:24 +0800

    WIP: quick add

--- client/src/components/logPage.js
+++ client/src/components/logPage.js
@@ -1,4 +1,4 @@
-function getLogPageRender({ config, logType, triggerType, platformName, direction, contactInfo, subject, note }) {
+function getLogPageRender({config, logType, triggerType, platformName, direction, contactInfo, subject, note }) {
     const additionalChoiceFields = logType === 'Call' ?
         config.platforms[platformName].page?.callLog?.additionalFields?.filter(f => f.type === 'selection') ?? [] :
         config.platforms[platformName].page?.messageLog?.additionalFields?.filter(f => f.type === 'selection') ?? [];
@@ -235,7 +235,7 @@ function getLogPageRender({ config, logType, triggerType, platformName, directio
     return page;
 }
 
-function getUpdatedLogPageRender({ config, logType, platformName, updateData }) {
+function getUpdatedLogPageRender({config, logType, platformName, updateData }) {
     const updatedFieldKey = updateData.keys[0];
     let page = updateData.page;
     // update target field value
