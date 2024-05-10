38cd781633ce154b75d6f5147ee00a4c287f7a10
Da Kong <kongda9999@gmail.com>
2024-05-10 16:02:19 +0800

    fix: limit contact sync call

--- client/src/popup.js
+++ client/src/popup.js
@@ -716,6 +716,8 @@ function getServiceConfig(serviceName) {
   const services = {
     name: serviceName,
     contactMatchPath: '/contacts/match',
+    contactMatchTtl: 7 * 24 * 60 * 60 * 1000, // contact match cache time in seconds, set as 7 days
+    contactNoMatchTtl: 7 * 24 * 60 * 60 * 1000, // contact no match cache time in seconds, default is 5 minutes, from v1.10.2
 
     // show auth/unauth button in ringcentral widgets
     authorizationPath: '/authorize',
