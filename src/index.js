c2ec5f1089fa67fa034bda1cd9d2b189a52ad921
SushilMallRC <sushil.mall@ringcentral.com>
2024-06-03 12:09:16 +0530

    Resolve callback URI

--- src/index.js
+++ src/index.js
@@ -136,7 +136,7 @@ app.get('/oauth-callback', async function (req, res) {
             platform,
             hostname,
             tokenUrl,
-            callbackUri:platform==="netsuite"?req.query.callbackUri+"&code="+req.query.code:req.query.callbackUri,
+            callbackUri:req.query.callbackUri,
             apiUrl: req.query.apiUrl,
             username: req.query.username
         });
