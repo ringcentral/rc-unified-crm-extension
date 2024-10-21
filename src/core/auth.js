bcce1fa71f7875da25fedb7892f16fcc209ca4a0
Da Kong <kongda9999@gmail.com>
2024-10-21 10:57:23 +0800

    fix: migrate bullhorn users to masterUserID

--- src/core/auth.js
+++ src/core/auth.js
@@ -2,6 +2,21 @@ const oauth = require('../lib/oauth');
 const { UserModel } = require('../models/userModel');
 const Op = require('sequelize').Op;
 
+async function tempMigrateBullhornUserId({ oldUserId }) {
+    const existingUser = await UserModel.findOne({
+        where: {
+            id: oldUserId,
+            platform: 'bullhorn'
+        }
+    });
+    if (existingUser) {
+        const platformModule = require(`../adapters/bullhorn`);
+        const newUserInfo = await platformModule.tempMigrateUserId({ existingUser });
+        return newUserInfo;
+    }
+    return null;
+}
+
 async function onOAuthCallback({ platform, hostname, tokenUrl, callbackUri, apiUrl, username, query }) {
     const platformModule = require(`../adapters/${platform}`);
     const oauthInfo = await platformModule.getOauthInfo({ tokenUrl, hostname, rcAccountId: query.rcAccountId });
@@ -124,3 +139,4 @@ async function saveUserInfo({ platformUserInfo, platform, hostname, accessToken,
 
 exports.onOAuthCallback = onOAuthCallback;
 exports.onApiKeyLogin = onApiKeyLogin;
+exports.tempMigrateBullhornUserId = tempMigrateBullhornUserId;
