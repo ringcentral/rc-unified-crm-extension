b59e16d91f4c74445d81572374965dceb7f6e730
Da Kong <kongda9999@gmail.com>
2024-12-25 16:49:22 +0800

    add new column for server side logging

--- src/models/adminConfigModel.js
+++ src/models/adminConfigModel.js
@@ -8,10 +8,13 @@ exports.AdminConfigModel = sequelize.define('adminConfigs', {
     type: Sequelize.STRING,
     primaryKey: true,
   },
-  userSettings:{
+  userSettings: {
     type: Sequelize.JSON
   },
-  customAdapter:{
+  customAdapter: {
+    type: Sequelize.JSON
+  },
+  serverSideLogging: {
     type: Sequelize.JSON
   }
 });
