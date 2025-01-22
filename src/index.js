662fff15a823df7a447b59954077a88c65c3e6a0
SushilMallRC <sushil.mall@ringcentral.com>
2025-01-22 10:09:32 +0530

    Updates error messages

--- src/index.js
+++ src/index.js
@@ -607,7 +607,7 @@ app.get('/contact', async function (req, res) {
             const { successful, returnMessage, contact, extraDataTracking } = await contactCore.findContact({ platform, userId, phoneNumber: req.query.phoneNumber, overridingFormat: req.query.overridingFormat, isExtension: req.query?.isExtension ?? false });
             res.status(200).send({ successful, returnMessage, contact });
             if (successful) {
-                const nonNewContact = contact.filter(c => !c.isNewContact);
+                const nonNewContact = contact?.filter(c => !c.isNewContact) ?? [];
                 resultCount = nonNewContact.length;
                 success = true;
                 if (!!extraDataTracking) {
