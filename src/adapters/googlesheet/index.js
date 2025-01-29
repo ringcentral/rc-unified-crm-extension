8a2674237cfd02b4d3cd4e0d20cc58d364df2c05
SushilMallRC <sushil.mall@ringcentral.com>
2025-01-29 23:23:05 +0530

    CreateCall Log , editCallLog, updateCallLog for Google Sheet Adapter

--- src/adapters/googlesheet/index.js
+++ src/adapters/googlesheet/index.js
@@ -104,8 +104,8 @@ async function createCallLog({ user, contactInfo, authHeader, callLog, note, add
 
     console.log({ user, contactInfo, authHeader, callLog, note, additionalSubmission, aiNote, transcript });
     const spreadsheetId = contactInfo.id;
-    const sheetName = contactInfo.name;
-    const range = `Sheet1!A1:append`;
+    const sheetName = 'Sheet1';
+    const range = `${sheetName}!A1:append`;
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?valueInputOption=RAW`;
 
     const headers = {
@@ -115,16 +115,26 @@ async function createCallLog({ user, contactInfo, authHeader, callLog, note, add
     const title = callLog.customSubject ?? `${callLog.direction} Call ${callLog.direction === 'Outbound' ? 'to' : 'from'} ${contactInfo.name}`;
     let callStartTime = moment(callLog.startTime).toISOString();
     const callEndTime = (callLog.duration === 'pending') ? moment(callStartTime) : moment(callStartTime).add(callLog.duration, 'seconds');
+    const spreadsheetData = await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}`, {
+        headers: {
+            'Authorization': `Bearer ${user.accessToken}`,
+            'Content-Type': 'application/json',
+        }
+    });
+    const nextLogRow = spreadsheetData.data.values.length + 1;
+    console.log({ spreadsheetData, nextLogRow });
     const data = {
         values: [
-            [title, contactInfo.phoneNumber, callStartTime, callEndTime, note]
+            [spreadsheetId + "space" + nextLogRow, title, contactInfo.phoneNumber, callStartTime, callEndTime, note]
         ],
     };
+
+
     try {
         const response = await axios.post(url, data, { headers });
         console.log('Response:', response.data);
         return {
-            logId: spreadsheetId,
+            logId: spreadsheetId + "space" + nextLogRow,
             returnMessage: {
                 message: 'Call logged',
                 messageType: 'success',
@@ -142,10 +152,135 @@ async function createCallLog({ user, contactInfo, authHeader, callLog, note, add
         }
     }
 }
+async function updateCallLog({ user, existingCallLog, authHeader, recordingLink, subject, note, startTime, duration, result, aiNote, transcript }) {
+    //console.log({ user, existingCallLog, authHeader, recordingLink, subject, note, startTime, duration, result, aiNote, transcript });
+    const existingLogId = existingCallLog.thirdPartyLogId;
+    const splitValues = existingLogId.split("space");
+    console.log({ splitValues, a: splitValues[0], b: splitValues[1] });
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${splitValues[0]}/values/Sheet1`;
 
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: authHeader,
+        },
+    });
+
+    const rows = response.data.values;
+    console.log({ rows });
+    // // Find the record where Name matches searchKey
+    // const header = rows[0];
+    // const nameIndex = header.indexOf("ID");
+    // const record = rows.find(row => row[nameIndex] === callLogId);
+    let rowIndex = -1;
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i][0] === existingLogId) { // Assuming column A is index 0
+            rowIndex = i + 1; // Convert to 1-based index (Sheets starts from 1)
+            break;
+        }
+    }
+    if (rowIndex === -1) {
+        return {
+            returnMessage: {
+                messageType: 'danger',
+                message: 'Call log not updated',
+                ttl: 3000
+            }
+        }
+    } else {
+        const response = await axios.post(
+            `https://sheets.googleapis.com/v4/spreadsheets/${splitValues[0]}/values:batchUpdate`,
+            {
+                valueInputOption: "RAW",
+                data: [
+                    {
+                        range: `Sheet1!B${rowIndex}`,
+                        values: [[subject]]
+                    },
+                    {
+                        range: `Sheet1!F${rowIndex}`,
+                        values: [[note]]
+                    }
+                ]
+            },
+            {
+                headers: {
+                    "Authorization": authHeader,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+        return {
+            updatedNote: note,
+            returnMessage: {
+                message: 'Call log updated.',
+                messageType: 'success',
+                ttl: 2000
+            }
+        };
+    }
+
+}
+
+async function getCallLog({ user, callLogId, authHeader }) {
+    console.log({ user, callLogId, authHeader });
+    const splitValues = callLogId.split("space");
+    console.log({ splitValues, a: splitValues[0], b: splitValues[1] });
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${splitValues[0]}/values/Sheet1`;
+
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: authHeader,
+        },
+    });
+
+    const rows = response.data.values;
+    console.log({ rows });
+    // // Find the record where Name matches searchKey
+    // const header = rows[0];
+    // const nameIndex = header.indexOf("ID");
+    // const record = rows.find(row => row[nameIndex] === callLogId);
+    let rowIndex = -1;
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i][0] === callLogId) { // Assuming column A is index 0
+            rowIndex = i + 1; // Convert to 1-based index (Sheets starts from 1)
+            break;
+        }
+    }
+    if (rowIndex === -1) {
+        return {
+            returnMessage: {
+                messageType: 'danger',
+                message: 'Call log not found',
+                ttl: 3000
+            }
+        }
+    } else {
+        const resultResponse = await axios.get(
+            `https://sheets.googleapis.com/v4/spreadsheets/${splitValues[0]}/values:batchGet?ranges=Sheet1!B${rowIndex}&ranges=Sheet1!F${rowIndex}`,
+            {
+                headers: { Authorization: authHeader }
+            }
+        );
+        const result = resultResponse.data.valueRanges;
+        const subject = result[0].values[0][0];
+        const note = result[1].values[0][0];
+        return {
+            callLogInfo: {
+                subject,
+                note,
+                additionalSubmission: {}
+            },
+            returnMessage: {
+            }
+        }
+    }
+
+}
 exports.getAuthType = getAuthType;
 exports.getOauthInfo = getOauthInfo;
 exports.getUserInfo = getUserInfo;
 exports.unAuthorize = unAuthorize;
 exports.findContact = findContact;
 exports.createCallLog = createCallLog;
+exports.updateCallLog = updateCallLog;
+exports.getCallLog = getCallLog;
