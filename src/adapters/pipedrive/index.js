b9e19fc87f7b06e9e58beb215b42ab49872a8aa2
Da Kong <kongda9999@gmail.com>
2025-05-15 09:44:39 +0800

    fix: Pipedrive duration wrong format

--- src/adapters/pipedrive/index.js
+++ src/adapters/pipedrive/index.js
@@ -209,13 +209,13 @@ async function createContact({ user, authHeader, phoneNumber, newContactName }) 
 
 function secondsToHoursMinutesSecondsInPipedriveFormat(seconds) {
     const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const secs = seconds % 60;
+    const minutes = Math.floor((seconds % 3600) / 60) + 1;
 
     if (hours > 0) {
-        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
-    } else {
-        return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+    }
+    else {
+        return `00:${String(minutes).padStart(2, '0')}`;
     }
 }
 
