5ab7fe3f36b2680d8ccd15b237f1e611a1e37bea
Da Kong <kongda9999@gmail.com>
2025-05-16 13:36:44 +0800

    fix: phone number format

--- src/index.js
+++ src/index.js
@@ -677,7 +677,7 @@ app.get('/contact', async function (req, res) {
         if (jwtToken) {
             const { id: userId, platform } = jwt.decodeJwt(jwtToken);
             platformName = platform;
-            const { successful, returnMessage, contact, extraDataTracking } = await contactCore.findContact({ platform, userId, phoneNumber: req.query.phoneNumber, overridingFormat: req.query.overridingFormat, isExtension: req.query?.isExtension ?? false });
+            const { successful, returnMessage, contact, extraDataTracking } = await contactCore.findContact({ platform, userId, phoneNumber: req.query.phoneNumber.replace(' ', '+'), overridingFormat: req.query.overridingFormat, isExtension: req.query?.isExtension ?? false });
             res.status(200).send({ successful, returnMessage, contact });
             if (successful) {
                 const nonNewContact = contact?.filter(c => !c.isNewContact) ?? [];
