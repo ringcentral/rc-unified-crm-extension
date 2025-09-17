c89cd8315bda3cfe23137a722bd5756c443e41a7
Da Kong <kongda9999@gmail.com>
2025-09-17 14:05:13 +0800

    add: clio, insightly, pipedrive, redtail

--- src/adapters/redtail/index.js
+++ src/adapters/redtail/index.js
@@ -512,6 +512,7 @@ function formatContact(rawContactInfo, categories) {
         phone: rawContactInfo.phoneNumber,
         title: rawContactInfo.job_title ?? "",
         type: 'contact',
+        mostRecentActivityDate: rawContactInfo.updated_at,
         additionalInfo: {
             category: categories.map(c => {
                 return {
